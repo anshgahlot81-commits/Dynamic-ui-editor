45098e3524ef86dfdef00eae98985821c2239b2c
Ansh Gahlot <anshgahlot81@gmail.com>
2025-10-19 00:39:57 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,5 @@ export default defineConfig({
 		jsx: "automatic", // use React 17+ automatic runtime
 		jsxImportSource: "react", // ensures the right import source
 	},
+	base: 'Dynamic-ui-editor',
 });
